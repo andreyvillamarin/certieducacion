0918bcec3a870cfdbeae87dd8642308a8ba0b844
andreyvillamarin <andreyvillamarin@gmail.com>
2025-07-22 15:27:57 -0500

    sendinblue
    clave sendinblue eliminada

--- config.php
+++ config.php
@@ -16,17 +16,17 @@ define('BASE_URL', 'https://qdos.network/demos/certieducacion2/');
 define('WHATSAPP_SUPPORT_NUMBER', '573204615527');
 
 // -- 3. CONFIGURACIÓN DE ENVÍO DE CORREO (BREVO SMTP) --
-define('BREVO_SMTP_HOST', 'smtp-relay.brevo.com');
-define('BREVO_SMTP_PORT', 587);
-define('BREVO_SMTP_USER', 'andreyvillamarin@gmail.com');
-define('BREVO_SMTP_KEY', 'xsmtpsib-5b2c4f62cf924e54e63d3914168bab52403f3b183783100593ba2d38a4276508-UkMmqZtw8SvjaT1X');
-define('SMTP_FROM_EMAIL', 'andreyvillamarin@gmail.com');
-define('SMTP_FROM_NAME', 'Comfamiliar');
+define('BREVO_SMTP_HOST', '');
+define('BREVO_SMTP_PORT', );
+define('BREVO_SMTP_USER', '');
+define('BREVO_SMTP_KEY', '');
+define('SMTP_FROM_EMAIL', '');
+define('SMTP_FROM_NAME', '');
 
 // -- 4. CONFIGURACIÓN DE ENVÍO DE SMS (ALTIRIA) --
-define('ALTIRIA_LOGIN', 'andreyvillamarin@gmail.com');
-define('ALTIRIA_PASSWORD', 'eb9xmheb');
-define('ALTIRIA_SENDER_ID', 'Comfamiliar');
+define('ALTIRIA_LOGIN', '');
+define('ALTIRIA_PASSWORD', '');
+define('ALTIRIA_SENDER_ID', '');
 
 // -- Configuración Interna (No modificar) --
 // -- 5. CONFIGURACIÓN DE SEGURIDAD --
